77e7ce71544ecd1c149d39cd6e47e0be3f2f2890
OrkaK <orkakalds3@gmail.com>
2025-02-06 18:03:24 -0500

    Removed all test files from tests/ folder

--- tests/tests/test_addition.py
+++
@@ -1,9 +0,0 @@
-"""Unit tests for addition function."""
-
-from app.calculator import add
-
-def test_add():
-    """Tests addition functionality."""
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
--- tests/tests/test_multiplication.py
+++
@@ -1,9 +0,0 @@
-"""Unit tests for multiplication function."""
-
-from app.calculator import multiply
-
-def test_multiply():
-    """Tests multiplication functionality."""
-    assert multiply(2, 3) == 6
-    assert multiply(-1, 5) == -5
-    assert multiply(0, 10) == 0
